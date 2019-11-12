c52ef313adcb158e875e7cee7825aab93358fdaa
Satya <satya.igu@gmail.com>
2019-11-12 23:33:49 +0530

    Pass object between two Activity

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,9 @@ n
 >app/src/main/java/com/satyasoft/kotlin_project/MainActivity.kt,c\0\c079a6f5541060fd4d4d976d47b2cff894fed582
 S
 #app/src/main/res/values/strings.xml,d\2\d2281fbb3027de2722081a53408dd77628bf080e
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+m
+=app/src/main/java/com/satyasoft/kotlin_project/SecondClass.kt,c\f\cf2c51e518c0d29b5c5214e17aa9a48cc998f05c
--- app/src/main/java/com/satyasoft/kotlin_project/MainActivity.kt
+++ app/src/main/java/com/satyasoft/kotlin_project/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.satyasoft.kotlin_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var name :TextView
+    private lateinit var password : TextView
+    private lateinit var save : Button
+    private lateinit var message:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        name = findViewById(R.id.name)
+        password = findViewById(R.id.password)
+        message = name.text.toString()
+        save = findViewById(R.id.saveButton)
+        save.setOnClickListener { sendMessage() }
 
+    }
+    /** Called when the user taps the Send button  */
+    private fun sendMessage() {
 
+        val intent = Intent(this,SecondActivity::class.java) //not application context
+        intent.putExtra("input",name.text.toString())
+        startActivity(intent)
     }
 }
